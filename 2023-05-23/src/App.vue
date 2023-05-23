eb3fd797e3d23bd66148e259929cea1c8ee53aa8
ferniemann <hello@ferdinand-niemann.eu>
2023-05-23 13:53:37 +0200

    initial commit

---
+++ 2023-05-23/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <PageHeader />
+</template>
+<script>
+import PageHeader from "@/components/PageHeader.vue";
+
+export default {
+  components: {
+    PageHeader,
+  },
+};
+</script>
---
+++ 2023-05-23/src/components/PageHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1>Bookmonkey</h1>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  color: cadetblue;
+}
+</style>
