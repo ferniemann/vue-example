2f4c655e81f9798eaee0722a2c695f299ed6d68f
ferniemann <hello@ferdinand-niemann.eu>
2023-05-24 15:53:07 +0200

    added files

---
+++ 2023-05-23/src/components/BookItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>{{ bookTitle }}</h2>
+    <p>{{ isbn }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    bookTitle: String,
+    isbn: String,
+  },
+};
+</script>
